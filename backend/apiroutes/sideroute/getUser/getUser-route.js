8de86bcbb2238498e53b3bc9e7f16f72377c3ebe
Mittelblut9 <sauer.benedikt@outlook.de>
2022-07-25 22:36:20 +0200

    Added google signin

--- backend/apiroutes/sideroute/getUser/getUser-route.js
+++ backend/apiroutes/sideroute/getUser/getUser-route.js
@@ -1,3 +1,12 @@
+const nconf = require('nconf');
+
+const jwt_decode = require('jwt-decode');
+
 module.exports = ({app}) => {
-    
+    app.get(nconf.get('routes:getUser:path'), (req, res) => {
+        const token = req.params.token;
+
+        const decoded = jwt_decode(token);
+        console.log(decoded);
+    })
 }
